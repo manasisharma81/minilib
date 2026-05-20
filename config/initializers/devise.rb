020ef8b29b41c8351c38361ff2e497940de569a8
manasisharma81 <manasi.sharma@chicagobooth.edu>
2026-05-20 04:30:27 +0000

    progress check in

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -15,7 +15,7 @@ Devise.setup do |config|
   config.expire_all_remember_me_on_sign_out = true
 
   config.password_length = 6..128
-  config.email_regexp = /A[^@ ]+@[^@ ]+z/
+  config.email_regexp = /\A[^@\s]+@[^@\s]+\z/
 
   config.reset_password_within = 6.hours
   config.sign_out_via = :delete
